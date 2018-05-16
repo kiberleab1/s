60e32a6acd7ea01e8f12c99b4c507f08f057e6a5
kiberleab <kiberleabeniyew@gmail.com>
2018-05-16 03:01:36 -0700

    first

---
+++ src/dial.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author eniyew
+ */
+public class call{
+    private String No;
+    private boolean Status;
+    private List<String> messages=new ArrayList();
+    
+    public void call(String No){
+        
+    }
+    public void leaveMessage(String messages){
+        
+    }
+}
+
---
+++ src/mes/voiemail.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mes;
+
+import static java.util.Objects.hash;
+
+/**
+ *
+ * @author eniyew
+ */
+public class voiemail {
+    private hash messageDial;
+    public List<String> myMessages(int no){
+        return messageDial[no];
+    }
+    public void addMessage(message mes,)
+}
